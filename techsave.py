31aaaf118dbb9d05e291e2fd860d3f73dc22f837
darkki <baka.prototype@gmail.com>
2020-05-13 13:51:59 +0300

    added component class

--- techsave.py
+++ techsave.py
@@ -12,3 +12,20 @@ import os # module imported for checking if file exists
 from os import path
 
 import pickle # module imported for loading/saving dictionaries/objects
+
+class app_info: # basic app info like version
+    version = "13052020|Dev"
+    by = "darkk!"
+
+class component:
+    def __init__(self, priority, type, url, name, price):
+        self.log(f"new component created! - [{priority}] {price}€ | {type} / {name} / {url}")
+        self.priority = priority
+        self.type = type
+        self.url = url
+        self.name = name
+        self.price = price
+    def log(self, message):
+        log_append = open("log.txt", "a")
+        log_append.write(f"[ {date_time_now} ] - [ {message} ]\n")
+        log_append.close()
