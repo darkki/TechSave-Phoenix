d9872d5f223ea21cbd43ba21d63b7fb47e288358
darkki <baka.prototype@gmail.com>
2020-05-13 14:59:40 +0300

    added basic initialization

--- techsave.py
+++ techsave.py
@@ -14,6 +14,7 @@ from os import path
 import pickle # module imported for loading/saving dictionaries/objects
 
 class app_info: # basic app info like version
+    name = "TechSave Phoenix"
     version = "13052020|Dev"
     by = "darkk!"
 
@@ -52,3 +53,25 @@ class money:
 def log(message):
     log_append = open("log.txt", "a")
     log_append.write(f"[ {date_time_now} ] - [ {message} ]\n")
+
+tic = time.time()
+print(f"[{Fore.CYAN}\tsp\init{Style.RESET_ALL}] beginning {app_info.name} v{app_info.version} initialization ... [{Fore.GREEN}READY{Style.RESET_ALL}]")
+print(f"[{Fore.CYAN}\tsp\init\data_load\check{Style.RESET_ALL}] checking if .\data.tsp exists ... ", end="")
+if path.isfile("./data.tsp") == True:
+    print(f"[{Fore.GREEN}FOUND{Style.RESET_ALL}]")
+    print(f"[{Fore.CYAN}\tsp\init\data_load\load{Style.RESET_ALL}] loading .\data.tsp ... ", end="")
+    tsp_dict = pickle.load(open("./data.tsp", "rb"))
+    print(f"[{Fore.GREEN}OK{Style.RESET_ALL}]")
+else:
+    print(f"[{Fore.RED}NOT_FOUND{Style.RESET_ALL}]")
+    print(f"[{Fore.CYAN}\tsp\init\data_create\init{Style.RESET_ALL}] initializing default values  ... ", end="")
+    tsp_dict = {}
+    tsp_dict["components"] = []
+    tsp_dict["finances"] = money(0)
+    print(f"[{Fore.GREEN}OK{Style.RESET_ALL}]")
+    print(f"[{Fore.CYAN}\tsp\init\data_create\save{Style.RESET_ALL}] saving .\data.tsp ... ", end="")
+    pickle.dump(tsp_dict, open("./data.tsp", "wb"))
+    print(f"[{Fore.GREEN}OK{Style.RESET_ALL}]")
+toc = time.time()
+tictoc = round(toc - tic, 2)
+print(f"[{Fore.CYAN}\tsp\init{Style.RESET_ALL}] initialization completed in {Style.BRIGHT}{tictoc}s{Style.RESET_ALL}, welcome to {Style.BRIGHT}{app_info.name} v{app_info.version} {Style.RESET_ALL}by {Style.BRIGHT}{app_info.by}{Style.RESET_ALL}")
