25f821918650b8fc1d7ef5ab3f74a22464a4e3e2
darkki <baka.prototype@gmail.com>
2020-05-13 13:36:33 +0300

    imports added

---
+++ techsave.py
@@ -0,0 +1,14 @@
+import datetime # module imported for date/time
+date_time_now = datetime.datetime.now()
+import time # module imported for tictoc
+
+import operator # module imported for sorting dictionaries
+
+from colorama import init # module imported for color support
+init()
+from colorama import Fore,Back,Style
+
+import os # module imported for checking if file exists
+from os import path
+
+import pickle # module imported for loading/saving dictionaries/objects
